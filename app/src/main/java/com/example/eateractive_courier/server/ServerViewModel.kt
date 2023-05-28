7ecf4f0446000ee3961229994c644aaa5540ff47
Cosmin Petolea <cosp@bendingspoons.com>
2023-05-28 23:48:43 +0300

    Updated Hostname

--- app/src/main/java/com/example/eateractive_courier/server/ServerViewModel.kt
+++ app/src/main/java/com/example/eateractive_courier/server/ServerViewModel.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServerViewModel {
-    private const val baseUrl = "http://vlogoeats.onrender.com"
+    private const val baseUrl = "http://x72bh6y9v-1zq4d3fpl.hopto.org:5000"
 
     fun getInstance(): Retrofit = Retrofit.Builder()
         .baseUrl(baseUrl)
